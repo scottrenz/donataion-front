f123af642b41d678f5954408cbf339b470013961
Scott Renz <scottrenz@scottrenz.com>
2019-07-17 22:55:28 -0400

    shows NASA photo or video of the day

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-// import React from "react"
 import React, { useEffect, useState } from "react"
 import Cards from './Cards'
 import Logo from './Logo'
 import axios from 'axios'
-// import axios from "./axios"
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
-
 
 function App() {
   const [imgData, setData] = useState(0)
-  // useEffect(() => {}, []); // Not synced with any data, so this effect only fires once
-  // function getData() {
   
     useEffect(() => {
-      https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH
+      // https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH')
       // axios.get('https://api.nasa.gov/planetary/apod?api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH&date=2012-03-14')
-      axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH')
+      // https://epic.gsfc.nasa.gov/api/enhanced/date/2015-10-31
+      // axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=awlI2QgBkvZtjasfaTqkmYUtoyGU6vfQLcsgQVmH')
+      // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+      // axios.get('https://epic.gsfc.nasa.gov/api/enhanced/date/2015-10-31')
       .then (data =>   
         {
-    console.log(data)
-    setData(data) 
-
-    // .then(res => setDogPic(res.data.message))
-
-    //  makeTabs(data.data.topics)
+          setData(data.data) 
        }
     )
       .catch (data => { 
@@ -34,23 +24,23 @@ function App() {
               }
                )
               }, []);
+    const style ={
+      display: 'flex',
+      marginTop: '10px',
+    }
+    const hStyle ={
+    justifyContent: 'center',
+    margin: '100px',
 
-    //           }
-    // getData();
-    
-
+    }
   return (
     <div className="container">
-      <div className='top'>
+      <div className='top' style={style}>
         <Logo />
       </div>
-      <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-
-        {/* <Cards cards={cards} /> */}
-        {/* <Numbers display={display} setDisplay={setDisplay} />
-        <Operators display={display} setDisplay={setDisplay} />
-        <Specials display={display} setDisplay={setDisplay} /> */}
+      <div className="App" >
+        <h1 style={hStyle}>NASA Image of the Day</h1>
+        <Cards date={imgData.date} expl={imgData.explanation} src={imgData.hdurl} url={imgData.url}/>
       </div>
     </div>
   )
--- src/Cards.js
+++ src/Cards.js
@@ -1,5 +1,19 @@
 import React from "react"
 
 export default props => {
-  return <div className='cards'>{props.cards}</div>
+  const cStyle = {
+    display: 'flex',
+    margin: '100px',
+    flexDiretion: 'column',
+    justifyContent: 'space-around',
+  }
+
+  let cHtml= props.src ? (<img alt='photo' src={props.src} />) : (<a href={props.url}>VIDEO</a>);
+  return (
+<div style={cStyle}>
+    <div className='display'>{props.date}</div>
+    {cHtml}
+    <div className='display'>{props.expl}</div>
+</div>
+  )   
 }
